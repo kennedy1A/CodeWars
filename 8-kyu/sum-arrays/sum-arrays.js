e167f0bf7bb54babbd877bdeecd6674ec7bbb3bf
Kennedy Mungwarakarama <kennedy.mungwarakarama24snhu@keplercollege.ac.rw>
2026-05-11 12:10:33 +0200

    Update solution - CodeHub

--- 8-kyu/sum-arrays/sum-arrays.js
+++ 8-kyu/sum-arrays/sum-arrays.js
@@ -1,13 +1,7 @@
-function sum(numbers){
-  let total =0
-    if (numbers.length === 0){
-    return 0
-      }
-  for(let i=0; i < numbers.length; i++){
-    total += numbers[i]
-  }
-  return total;
-  }
-​
-​
-  
+function sum (numbers) {let total=0;
+                        if(numbers.length===0){
+                          return 0;
+                        } for (let i=0; i< numbers.length; i++){
+                          total += numbers[i]
+                        }return total
+}
