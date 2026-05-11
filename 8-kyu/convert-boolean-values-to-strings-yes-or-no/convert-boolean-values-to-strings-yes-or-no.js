10b2270a83638a54abc690bebc93caf38ecfee6e
Kennedy Mungwarakarama <kennedy.mungwarakarama24snhu@keplercollege.ac.rw>
2026-05-11 12:16:07 +0200

    Add solution - CodeHub

---
+++ 8-kyu/convert-boolean-values-to-strings-yes-or-no/convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,14 @@
+//If condition to check for a boolean value 
+// return a condition
+function boolToWord( bool ){
+  if(bool === true){
+    return "Yes"
+  }
+  else if(bool === false){
+    return "No"
+  }
+  else {
+    return ""
+      }
+  }
+​
