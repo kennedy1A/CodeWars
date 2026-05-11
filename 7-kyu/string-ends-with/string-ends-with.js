c5a0c3342b6fa0358fa3780b209efe2875a1fbad
Kennedy Mungwarakarama <kennedy.mungwarakarama24snhu@keplercollege.ac.rw>
2026-05-11 12:19:35 +0200

    Add solution - CodeHub

---
+++ 7-kyu/string-ends-with/string-ends-with.js
@@ -0,0 +1,13 @@
+//Approach 1 compliting the solution to return true for a string passed
+//And The second string (second argument must be exactly what a string ends with)
+//Approach we have to check a string ending argument using endwith() method
+//This will check if the value in ending is in the string passed. 
+// Step one if condition to check if str end with endsWith(ending)
+function solution(str, ending){
+  if(str.endsWith(ending)){
+    return true
+  }else{
+    return false
+  }
+  // TODO: complete
+}
