210b3ca3d093a41795b5242c52098cfd0070f8e6
Kennedy Mungwarakarama <kennedy.mungwarakarama24snhu@keplercollege.ac.rw>
2026-05-11 12:21:38 +0200

    Update solution - CodeHub

--- 8-kyu/get-the-mean-of-an-array/get-the-mean-of-an-array.js
+++ 8-kyu/get-the-mean-of-an-array/get-the-mean-of-an-array.js
@@ -1,11 +1,9 @@
-​
-  //TODO : calculate the downward rounded average of the marks array
-​
 function getAverage(marks){
   let total = 0;
-  for(let i = 0; i < marks.length; i++){
-    total += marks[i];
-  }
-  return Math.floor(total/marks.length);
+  for (let i=0; i < marks.length; i++) 
+  {
+  total+= marks[i]/marks.length}
+  return Math.floor(total)
 }
+​
 ​
